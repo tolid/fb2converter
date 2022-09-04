19091c0c62ef746183e1fc0af7b3eae5086a20f7
rupor-github <rupor0mail@gmail.com>
2022-09-04 12:15:47 -0400

    Contibue to move converting code

--- archive/walker.go
+++ archive/walker.go
@@ -3,6 +3,9 @@ package archive
 
 import (
 	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -27,6 +30,54 @@ func Walk(archive, pattern string, walkFn WalkFunc) error {
 			if err := walkFn(archive, f); err != nil {
 				return err
 			}
+		}
+	}
+	return nil
+}
+
+// Unzip completely unpacks archive into destination directory.
+func Unzip(archive, dest string) error {
+
+	r, err := zip.OpenReader(archive)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	extract := func(f *zip.File) error {
+
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+
+		path := filepath.Join(dest, f.Name)
+
+		if f.FileInfo().IsDir() {
+			if err := os.MkdirAll(path, 0700); err != nil {
+				return err
+			}
+		} else {
+			if err := os.MkdirAll(filepath.Dir(path), 0700); err != nil {
+				return err
+			}
+			f, err := os.Create(path)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			if _, err = io.Copy(f, rc); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		if err := extract(f); err != nil {
+			return err
 		}
 	}
 	return nil
--- processor/kfx/convert.go
+++ processor/kfx/convert.go
@@ -2,12 +2,35 @@ package kfx
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 
 	"go.uber.org/zap"
 
+	"fb2converter/archive"
 	"fb2converter/state"
 )
+
+const (
+	DirKdf = "KDF"
+)
+
+type cnvrtr struct {
+	log *zap.Logger
+}
+
+func (c *cnvrtr) unpackKpf(kpf, kdf string) error {
+
+	if err := os.MkdirAll(kdf, 0700); err != nil {
+		return fmt.Errorf("unable to create directories for KDF contaner: %w", err)
+	}
+	// unwrapping KPF which is zipped KDF
+	if err := archive.Unzip(kpf, kdf); err != nil {
+		return fmt.Errorf("unable to unzip KDF contaner (%s): %w", kpf, err)
+	}
+	return nil
+}
 
 // ConvertFromKpf() takes KPT file and re-packs it to KFX file sutable for Kindle.
 func ConvertFromKpf(fromKpf, toKfx, outDir string, env *state.LocalEnv) error {
@@ -21,6 +44,14 @@ func ConvertFromKpf(fromKpf, toKfx, outDir string, env *state.LocalEnv) error {
 			zap.String("to", toKfx),
 		)
 	}(start)
+
+	c := cnvrtr{
+		log: env.Log,
+	}
+
+	if err := c.unpackKpf(fromKpf, filepath.Join(outDir, DirKdf)); err != nil {
+		return err
+	}
 
 	return fmt.Errorf("FIX ME DONE: ConvertFromKpf")
 }
